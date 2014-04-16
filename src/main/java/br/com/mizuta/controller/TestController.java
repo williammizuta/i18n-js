2c5c5a1cf996944d53ca275d743c50148a71156d
William Seiti Mizuta <william.mizuta@gmail.com>
2014-04-16 01:11:44 -0300

    Creating a test page

---
+++ src/main/java/br/com/mizuta/controller/TestController.java
@@ -0,0 +1,14 @@
+package br.com.mizuta.controller;
+
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+public class TestController {
+
+	@Get("/test")
+	public void test() {
+
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/test/test.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<body>
+		<script src="http://zeptojs.com/zepto.js"></script>
+		<script src="/js/i18n.js"></script>
+	</body>
+</html>
